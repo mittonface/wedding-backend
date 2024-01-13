39e46a7fe5b77d5d8986ccd71e57653534df783c
Brent Mitton <brent.mitton@gmail.com>
2024-01-13 11:23:56 -0500

    forgot to commit new cors middeware

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -11,3 +11,11 @@ func LogMiddleware(next http.Handler) http.Handler {
 		next.ServeHTTP(w, r) // call the next handler, which can be another middleware or the final handler
 	})
 }
+
+
+func EnableCors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		next.ServeHTTP(w, r)
+	})
+ }
