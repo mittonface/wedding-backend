ac1f28f969daf61d38592033a43642501deaeb74
Brent Mitton <brent.mitton+github@gmail.com>
2024-01-28 21:36:47 -0500

    unbreak the tf variables

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -41,6 +41,8 @@ variable "supabase_url" {
 
 variable "supabase_secret" {
     description = "The api secret for our database"
+}
+
 variable "USE_SSL" {
     description = "Use ssl?"
     default = "true"
