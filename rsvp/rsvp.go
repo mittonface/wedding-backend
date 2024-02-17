957bdff08c3da68d07cb54b0801e5567e05ce49c
Brent Mitton <brent.mitton@gmail.com>
2024-02-17 13:32:45 -0500

    remove backend validaiton

--- rsvp/rsvp.go
+++ rsvp/rsvp.go
@@ -3,9 +3,9 @@ package rsvp
 type RSVP struct {
 	RsvpId string `json:"rsvpId" validate:"required"`
 	RsvpName string `json:"rsvpName"`
-	NumGuests int `json:"numGuests" validate:"required"`
+	NumGuests int `json:"numGuests"`
 	PlusOneName string `json:"plusOneName"`
-	MealSelection1 string `json:"mealSelection1" validate:"required"`
+	MealSelection1 string `json:"mealSelection1"`
 	MealSelection2 string `json:"mealSelection2"`
 	ExtraText string `json:"extraText"`
 	Attending bool `json:"attending"`
